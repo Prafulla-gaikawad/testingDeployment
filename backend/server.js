312c9d74290644344cd3a730c80fb341d10fc532
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-11-14 15:53:26 +0530

    pushed

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://testingdeployment-juen.onrender.com' || '*',
+  origin: 'https://fastidious-creponne-59066f.netlify.app/' || '*',
   credentials: true
 }));
 app.use(bodyParser.json());
